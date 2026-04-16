12aa4b897282321b8e825a8290981d46fc51af2b
mdsanzidhasnat <mdsanzidhasnat27@gmail.com>
2026-04-16 17:01:15 +0600

    logo

--- components/header/Logo.tsx
+++ components/header/Logo.tsx
@@ -5,13 +5,13 @@ export default function Logo() {
   return (
     <Link href="/" className="flex items-center shrink-0">
       <Image
-        src="/images/CDkeyDeals.com.png"
+        src="/images/CDkeyDeals Logo.svg"
         alt="CDKeyDeals Logo"
-        width={80}
-        height={24}
+        width={180}
+        height={33}
         className="h-7 w-auto object-contain"
         priority
-        quality={95}
+        quality={100}
         unoptimized={false}
         
         sizes="(max-width: 768px) 84px, 84px"
